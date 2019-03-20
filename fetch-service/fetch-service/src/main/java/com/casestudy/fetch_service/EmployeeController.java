9d7488e5d61defb3dc04ffe4dd2cbcc3bc7ebe6f
kanha11 <48746163+kanha11@users.noreply.github.com>
2019-03-20 12:51:03 +0530

    Add files via upload

---
+++ fetch-service/fetch-service/src/main/java/com/casestudy/fetch_service/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.casestudy.fetch_service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	@RequestMapping(value = "/employees", method = RequestMethod.GET, produces = "application/json")
+	public List<Employee> getEmployees() {
+		List<Employee> employees = new LinkedList<>();
+		employees.add(new Employee(1234, "employee@yodlee.com"));
+		employees.add(new Employee(2345, "abcd@yodlee.com"));
+		return employees;
+	}
+	
+	@RequestMapping(value = "/employee/{eid}", method = RequestMethod.GET, produces = "application/json")
+	public Employee getEmployee(@PathVariable Integer eid) {
+		return new Employee(eid, "employee@yodlee.com");
+	}
+
+}
